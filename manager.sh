f49bc2c80ec8370c3c0a6b25c289087dc207902b
anyarom <r0mannava@mail.ru>
2019-12-24 23:05:35 +0300

    updated Dockerfile

--- manager.sh
+++ manager.sh
@@ -11,24 +11,26 @@ cd "/Users/anya/IdeaProjects/messages"
 docker-compose up --force-recreate msg_postgres_local
 
 echo "acceptor"
-# сборка бинарника acceptor
+# сборка образа acceptor
 cd '/Users/anya/IdeaProjects/messages/acceptor'
-GOOS=linux GOARCH=amd64 go build -o deploy/app_acceptor
+# бинарник собираем внутри контейнера
+#GOOS=linux GOARCH=amd64 go build -o deploy/app_acceptor
 # создать образ
 docker build -f deploy/Dockerfile . -t siannarom/msg_acceptor
-rm deploy/app_acceptor
+#rm deploy/app_acceptor
 # локальный запуск
 cd "/Users/anya/IdeaProjects/messages"
 docker-compose up --force-recreate msg_acceptor
 
 
 echo 'msg_worker'
-# сборка бинарника msg_worker
+# сборка образа msg_worker
 cd '/Users/anya/IdeaProjects/messages/worker'
-GOOS=linux GOARCH=amd64 go build -o deploy/app_msg_worker
+# бинарник собираем внутри контейнера
+#GOOS=linux GOARCH=amd64 go build -o deploy/app_msg_worker
 # создать образ
 docker build -f deploy/Dockerfile . -t siannarom/msg_worker
-rm deploy/app_msg_worker
+#rm deploy/app_msg_worker
 # локальный запуск
 cd "/Users/anya/IdeaProjects/messages"
 docker-compose up --force-recreate msg_worker_local
